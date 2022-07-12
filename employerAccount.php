3dbee616bc02c839680b03187ebe5d15398a1e01
Kayden Lee <kayden@programmer.net>
2022-07-12 16:30:28 +0800

    update

--- employerAccount.php
+++ employerAccount.php
@@ -7,7 +7,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" href="img/favicon.svg" type="image/x-icon">
-<title> Account | Employer</title>
+<title>Account | Employer</title>
 
 <link href="css/simpleGridTemplate.css" rel="stylesheet" type="text/css">
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
