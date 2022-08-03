4769c65455178f00f5e25481fd04dc9dc3b329e6
Kayden Lee <kayden@programmer.net>
2022-08-03 07:29:08 +0000

    update

--- navBar.php
+++ navBar.php
@@ -41,7 +41,7 @@
    echo ' <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">'.$myusername.'<span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
             <li><a href="seekerAccount.php">My Profile</a></li>
-             <li><a href="AppliedJobs.php">Jobs Applied</a></li>
+             <li><a href="Appliedjobs.php">Jobs Applied</a></li>
              <li><a href="logout.php">Logout</a></li>
        
           </ul>
