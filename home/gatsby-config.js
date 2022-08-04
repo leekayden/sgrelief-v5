2cfb9cb7924ad4212731fd26983b852f87a36161
leekayden <kayden@programmer.net>
2022-08-04 19:01:21 +0800

    update

--- home/gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: `Landy-Gatsby-Starter`,
-    description: `Landy is a free React landing page template designed for developers and startups.`,
-  },
-  plugins: ["gatsby-plugin-react-helmet"],
-}
--- home/src/common/SvgIcon/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SvgIcon = ({ src, width, height }) => (
-  <img src={`/img/svg/${src}`} alt={src} with={width} height={height} />
-);
-
-export default SvgIcon;
