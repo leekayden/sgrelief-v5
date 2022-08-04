5470975debf8768e7bfe2cbd468c0e221e4c45e6
Kayden Lee <kayden@programmer.net>
2022-08-04 04:30:43 +0000

    update

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if (session_status() == PHP_SESSION_NONE) {
             </div>
             <div class="right-header">
                 <h1 class="name">
-                    Hello! Welcome to SG<span>relief</span>teaching!
+                    Welcome to SG<span>relief</span>teaching!
                 </h1>
                 <p>
                 At SG<span>relief</span>teaching, we help teachers who needs relief teachers that can carry out lessons for them. We also help relief teachers find jobs from teachers who require them as well!
