a9e444bc832a77efde84e4e30ed35bfbcc19e8a9
leekayden <kayden@programmer.net>
2022-08-11 15:58:53 +0800

    update

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if (session_status() == PHP_SESSION_NONE) {
                 </p>
                 <div class="btn-con">
                     <a href="/main.php?msg=login" class="main-btn">
-                        <span class="btn-text">Home</span>
+                        <span class="btn-text">Get started</span>
                         <span class="btn-icon"><i class="fas fa-solid fa-arrow-right"></i></span>
                     </a>
                 </div>
@@ -56,13 +56,6 @@ if (session_status() == PHP_SESSION_NONE) {
                     <div class="btn-con">
                         <a href="/main.php?msg=login" class="main-btn">
                             <span class="btn-text">Start now!</span>
-                            <span class="btn-icon"><i class="fas fa-solid fa-arrow-right"></i></span>
-                        </a>
-                    </div>
-                    <br>
-                    <div class="btn-con">
-                        <a href="/jobs/" class="main-btn">
-                            <span class="btn-text">Find jobs</span>
                             <span class="btn-icon"><i class="fas fa-solid fa-arrow-right"></i></span>
                         </a>
                     </div>
