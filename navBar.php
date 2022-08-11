46ef6fadb1e5ffafdd66d3bbe14a00c6a36c1c15
Kayden Lee <kayden@programmer.net>
2022-08-11 10:22:47 +0800

    Update navBar.php

--- navBar.php
+++ navBar.php
@@ -69,6 +69,7 @@
 </a></li>';
      
  }  ?>
+        <li><a href="https://www.moe.gov.sg/-/media/files/about-us/school-clusters-2022.ashx/">View school cluster list</a></li>
         <li><a href="/main.php">Find Jobs</a></li>
         <li><a href="/contact">Contact Us</a></li>
       </ul>
